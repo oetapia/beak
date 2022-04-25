16876ed1bcaa6a3a6e9a674c4b766f48cd080bc7
E <e@estebantapia.com>
2022-04-25 16:06:52 +0200

    cleaned up functions

--- js/beak.js
+++ js/beak.js
@@ -1,13 +1,33 @@
+
+// data to test JSON returns
+var data = '[{"a":1,"b":2,"c":3},{"a":4,"b":5,"c":6},{"a":7,"b":8,"c":9}]';
+
+// test function with props
 edit.onclick = (event) => {
 	event.preventDefault();
 	// show the selected index
 	alert(event);
 };
 
+
+// test function with onclick
 function editForm () {
 	alert('edit');
 }
 
-function sendAPI () {
-	console.log('sending');
+// parding data as JSON object 
+function addData(data) { 
+} 
+var data = JSON.parse(data);
+
+// function to replace content in HTML
+function fillData() {
+	data.forEach(e => console.log(e));
+	var tag = document.createElement("li");
+	var text = document.createTextNode(JSON.stringify(data));
+	tag.appendChild(text);
+	var element = document.getElementById("participants");
+	element.appendChild(tag);
+	addData(data);
 }
+
